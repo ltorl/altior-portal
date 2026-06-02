c38f1046b7d0c16b8168976dd28e86b4cdbb4872
ltorl <126298377+ltorl@users.noreply.github.com>
2026-06-02 11:06:59 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-importScripts("/scram/scramjet.all.js");
+importScripts("/scramjet/scram/scramjet.all.js");
 
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
