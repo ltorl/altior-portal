46fbd2f492e83ba4e9e3dcfd5ee48ce86dacf9e2
ltorl <126298377+ltorl@users.noreply.github.com>
2026-06-01 21:45:45 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,4 @@
 importScripts('https://cdn.jsdelivr.net/gh/ltorl/Scramjet-App@main/public/sw.js');
+importScripts('https://cdn.jsdelivr.net/gh/ltorl/Scramjet-App@main/public/scramjet/scramjet.config.js');
+importScripts('https://cdn.jsdelivr.net/gh/ltorl/Scramjet-App@main/public/scramjet/scramjet.codecs.js');
+importScripts('https://cdn.jsdelivr.net/gh/ltorl/Scramjet-App@main/public/scramjet/scramjet.worker.js');
