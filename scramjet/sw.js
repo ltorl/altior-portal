27b8f8b7a36689396e65d551615463e98face636
ltorl <126298377+ltorl@users.noreply.github.com>
2026-06-02 11:01:20 -0600

    moved sw

--- scramjet/sw.js
+++
@@ -1,4 +0,0 @@
-importScripts('https://cdn.jsdelivr.net/gh/ltorl/Scramjet-App@main/public/sw.js');
-importScripts('https://cdn.jsdelivr.net/gh/ltorl/Scramjet-App@main/public/scramjet/scramjet.config.js');
-importScripts('https://cdn.jsdelivr.net/gh/ltorl/Scramjet-App@main/public/scramjet/scramjet.codecs.js');
-importScripts('https://cdn.jsdelivr.net/gh/ltorl/Scramjet-App@main/public/scramjet/scramjet.worker.js');
