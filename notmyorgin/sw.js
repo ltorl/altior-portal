9ba48089f2463259abcde47ae370283358da3c1e
ltorl <126298377+ltorl@users.noreply.github.com>
2026-06-03 14:11:19 -0600

    changes

--- notmyorgin/sw.js
+++ notmyorgin/sw.js
@@ -1,4 +1,4 @@
-importScripts("/scram/scramjet.all.js");
+importScripts("https://altior-portal.onrender.com/scram/scramjet.all.js");
 
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
